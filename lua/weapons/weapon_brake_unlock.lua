f59b265e6b4fbf5b346f47fc9a77c2b9a129b579
LambdaGaming <starwars18m@gmail.com>
2019-07-12 16:12:29 -0400

    Update weapon_brake_unlock.lua

--- lua/weapons/weapon_brake_unlock.lua
+++ lua/weapons/weapon_brake_unlock.lua
@@ -23,13 +23,15 @@ SWEP.Secondary.DefaultClip = -1
 SWEP.Secondary.Automatic = false
 
 function SWEP:PrimaryAttack()
+	if !IsFirstTimePredicted() then return end
     local tr = self.Owner:GetEyeTrace().Entity
+	if self.Owner:GetPos():DistToSqr( tr:GetPos() ) > 90000 then return end
     if tr:GetClass() == "prop_vehicle_jeep" then
     	if SERVER then
     		tr:Fire( "HandBrakeOff", "", 0.01 )
     		self.Owner:ChatPrint( "Brakes released." )
     	end
-    	tr:EmitSound( "physics/metal/metal_solid_strain"..math.random( 1, 5 )..".wav" )
+    	tr:EmitSound( "physics/metal/metal_box_impact_soft"..math.random( 1, 3 )..".wav" )
     end
     self:SetNextPrimaryFire( CurTime() + 1 )
 end
