bedffc96db240f3cee7a55cf933e754f6bd64af1
LambdaGaming <starwars18m@gmail.com>
2021-12-19 23:29:15 -0500

    Update to latest gchroma version

--- lua/autorun/server/automod_gchroma_server.lua
+++ lua/autorun/server/automod_gchroma_server.lua
@@ -2,10 +2,8 @@ util.AddNetworkString( "AM_InitGChroma" )
 local function AM_InitGChroma( ply, veh )
 	if gchroma then
 		if !veh:GetNWBool( "IsAutomodSeat" ) and veh.seat then
-			local tbl = {
-				gchroma.ResetDevice( GCHROMA_DEVICE_ALL ),
-				gchroma.SetDeviceColor( GCHROMA_DEVICE_ALL, Vector( 25, 25, 25 ) )
-			}
+			gchroma.ResetDevice( GCHROMA_DEVICE_ALL )
+			gchroma.SetDeviceColor( GCHROMA_DEVICE_ALL, Vector( 25, 25, 25 ) )
 			for k,v in pairs( veh.seat ) do
 				local color
 				if IsValid( v:GetDriver() ) then
@@ -13,10 +11,10 @@ local function AM_InitGChroma( ply, veh )
 				else
 					color = GCHROMA_COLOR_WHITE
 				end
-				table.insert( tbl, gchroma.SetDeviceColorEx( GCHROMA_DEVICE_KEYBOARD, GCHROMA_COLOR_RED, GCHROMA_KEY_1, 0 ) )
-				table.insert( tbl, gchroma.SetDeviceColorEx( GCHROMA_DEVICE_KEYBOARD, color, _G["GCHROMA_KEY_"..k + 1], 0 ) ) --Seat keys will light up red if they are occupied
+				gchroma.SetDeviceColorEx( GCHROMA_DEVICE_KEYBOARD, GCHROMA_COLOR_RED, GCHROMA_KEY_1, 0 )
+				gchroma.SetDeviceColorEx( GCHROMA_DEVICE_KEYBOARD, color, _G["GCHROMA_KEY_"..k + 1], 0 ) --Seat keys will light up red if they are occupied
 			end
-			gchroma.SendFunctions( ply, tbl )
+			gchroma.SendFunctions( ply )
 			net.Start( "AM_InitGChroma" ) --Init the rest of the lights client-side to avoid sending an unnecessary amount of net messages
 			net.Send( ply )
 		end
@@ -29,9 +27,8 @@ local function AM_GChromaEnteredVehicle( ply, veh, role )
 			local parent = veh:GetParent()
 			local driver = parent:GetDriver()
 			if IsValid( driver ) then
-				local tbl = {}
-				table.insert( tbl, gchroma.SetDeviceColorEx( GCHROMA_DEVICE_KEYBOARD, GCHROMA_COLOR_RED, _G["GCHROMA_KEY_"..veh.ID + 1], 0 ) )
-				gchroma.SendFunctions( driver, tbl )
+				gchroma.SetDeviceColorEx( GCHROMA_DEVICE_KEYBOARD, GCHROMA_COLOR_RED, _G["GCHROMA_KEY_"..veh.ID + 1], 0 )
+				gchroma.SendFunctions( driver )
 			end
 		else
 			AM_InitGChroma( ply, veh )
@@ -46,8 +43,8 @@ local function AM_GChromaLeaveVehicle( ply, ent )
 			local parent = ent:GetParent()
 			local driver = parent:GetDriver()
 			if IsValid( driver ) then
-				local tbl = { gchroma.SetDeviceColorEx( GCHROMA_DEVICE_KEYBOARD, GCHROMA_COLOR_WHITE, _G["GCHROMA_KEY_"..ent.ID + 1], 0 ) }
-				gchroma.SendFunctions( driver, tbl )
+				gchroma.SetDeviceColorEx( GCHROMA_DEVICE_KEYBOARD, GCHROMA_COLOR_WHITE, _G["GCHROMA_KEY_"..ent.ID + 1], 0 )
+				gchroma.SendFunctions( driver )
 			end
 		else
 			if GChroma_PlayerModuleLoaded then
@@ -55,8 +52,8 @@ local function AM_GChromaLeaveVehicle( ply, ent )
 				net.Send( ply )
 				return
 			end
-			local tbl = { gchroma.ResetDevice( GCHROMA_DEVICE_ALL ) }
-			gchroma.SendFunctions( ply, tbl )
+			gchroma.ResetDevice( GCHROMA_DEVICE_ALL )
+			gchroma.SendFunctions( ply )
 		end
 	end
 end
