eb0b2a12ed7d0b08be4cdb65a833f8dc7fdfd7c5
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-10-24 19:51:00 -0400

    Merge cl_client.lua into cl_hud.lua

--- lua/gauto/client/cl_hud.lua
+++ lua/gauto/client/cl_hud.lua
@@ -1,3 +1,14 @@
+function GAuto.Notify( text )
+	local textcolor1 = Color( 180, 0, 0, 255 )
+	local textcolor2 = color_white
+	chat.AddText( textcolor1, "[GAuto]: ", textcolor2, text )
+end
+
+net.Receive( "GAuto_Notify", function()
+	local text = net.ReadString()
+	GAuto.Notify( text )
+end )
+
 surface.CreateFont( "GAuto_HUDFont1", {
 	font = "Roboto",
 	size = 18
